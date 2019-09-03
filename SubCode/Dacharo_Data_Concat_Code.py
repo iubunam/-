9b57367e67d54e2437cc68af36bd8e2ebab80eec
iubunam <iubunam@naver.com>
2019-09-03 22:48:24 +0900

    Add 이상치 제거 in Dachro_Data_Concat_Code

--- SubCode/Dacharo_Data_Concat_Code.py
+++ SubCode/Dacharo_Data_Concat_Code.py
@@ -8,7 +8,8 @@ Created on Tue Aug 27 23:35:16 2019
 import os
 import glob
 import pandas as pd
-
+from scipy import stats
+import numpy as 
 #%%
 files = glob.glob('DataFiles/*.csv')
 
@@ -19,20 +20,34 @@ dataset_total[['P_ID', 'S_Direction', 'S_Peak', 'S_Way']] = dataset_total['New']
 dataset_total.drop(columns = 'New', inplace = True)
 
 
-#%%
+#%% 
 
-
-#
-#dataset_total.loc[dataset_total['S_Way'] == '(2)', 'S_Way'] = 'M-Hipass'
-#
-#dataset_total.loc[dataset_total['S_Way'] == '(3)', 'S_Way'] = 'TCS'
-#
-#dataset_total.loc[dataset_total['S_Way'] == '(1)', 'S_Way'] = 'Right
-
-
+# 방향별 TTC정리
 dataset_total.loc[dataset_total['S_Direction'] == 'Sangheng', 'TTC'] = - dataset_total.loc[dataset_total['S_Direction'] == 'Sangheng', 'TTC']
 
+# TTC 마이너스값 처리
 dataset_total.loc[dataset_total['TTC'] < 0, 'TTC'] = 999
 
+#%% 이상치 제거
+# 이상치 찾기
+def FindOutlier(dataset_total, var, sigma):
+    outlierdataset = dataset_total.loc[(np.abs(stats.zscore(dataset_total[var])) > sigma), var]
+    return outlierdataset
+
+# 이상치 -> nan
+def FindOutlierNan(dataset_total, var, sigma):
+    dataset_total.loc[(np.abs(stats.zscore(dataset_total[var])) > sigma), var] = np.nan
+
+for i in dataset_total.columns[4:8]:
+    FindOutlierNan(dataset_total, i, 3)
+
+
+#%%
+# 데이터 Export
 dataset_total.to_csv('dataset_total.csv', index = False)
  
+
+
+
+
+
